660eceeaf45e9783485005c704eed73ee3bc3a4a
metatrontu@gmail.com <metatrontu@gmail.com>
2016-06-29 19:14:05 +0800

    Test first react component and prerender function

---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,17 @@
+//= require_tree ./components
+
+class Main extends React.Component {
+	render() {
+		return (
+			<h1> Hello From react and test prerender! </h1>
+		);
+	}
+}
+// let documentReady = () => {
+//   ReactDOM.render(
+//     <Main />,
+//     document.getElementById('react')
+//   );
+// };
+
+// $(documentReady);
