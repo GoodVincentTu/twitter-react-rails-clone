2ebd1965278c58976ab91a25be4f11e6268b3e18
metatrontu@gmail.com <metatrontu@gmail.com>
2016-06-30 20:03:34 +0800

    mockup tweets interaction

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,10 +1,31 @@
 import TweetBox from "./components/TweetBox.jsx"
+import TweetsList from "./components/TweetsList.jsx"
+
+// let mockTweets = [
+//   { id: 1, name: 'Samuel', body: '#First Tweet'},
+//   { id: 2, name: 'Samuel', body: '#First Tweet'},
+//   { id: 3, name: 'Samuel', body: '#First Tweet'}
+// ];
+
 
 class Main extends React.Component {
+	constructor(props){
+		super(props);
+		// this.state = { tweetsList: mockTweets }
+	  this.state = { tweetsList: []}
+	}
+	addTweet(tweetToAdd) {
+		//  mockTweets.unshift({...})
+		let newTweetsList = this.state.tweetsList;
+		newTweetsList.unshift({ id: Date.now(), name: 'Guest', body: tweetToAdd });
+
+		this.setState({ tweetsList: newTweetsList});
+	}
 	render() {
 		return (
 			<div className="containter">
-			  <TweetBox />
+			  <TweetBox sendTweet={this.addTweet.bind(this)}/>
+			  <TweetsList tweets={this.state.tweetsList}/>
 			</div>
 		);
 	}
